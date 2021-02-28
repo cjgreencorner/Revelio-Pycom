2f39a721458ea0d109767270eadc2389b94cd68c
cjgreencorner <74549603+cjgreencorner@users.noreply.github.com>
2021-02-28 15:51:30 +0100

    Final commit

--- boot.py
+++ boot.py
@@ -4,7 +4,7 @@ import pycom
 pycom.heartbeat(False)
 wlan = WLAN(mode=WLAN.STA)
 
-wlan.connect(ssid='telenet-82DF7B9', auth=(WLAN.WPA2, 'Yutebtzuz6a2'))
+wlan.connect(ssid='telenet-82DF7B9', auth=(WLAN.WPA2, 'password'))
 while not wlan.isconnected():
     machine.idle()
     pycom.rgbled(0xFF0000)
