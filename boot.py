25fdf52790e4d6968eb03682489aa74262ca7fd3
cjgreencorner <74549603+cjgreencorner@users.noreply.github.com>
2021-02-28 15:50:33 +0100

    Final commit

--- boot.py
+++ boot.py
@@ -4,7 +4,7 @@ import pycom
 pycom.heartbeat(False)
 wlan = WLAN(mode=WLAN.STA)
 
-wlan.connect(ssid='telenet-82DF7B9', auth=(WLAN.WPA2, 'Password'))
+wlan.connect(ssid='telenet-82DF7B9', auth=(WLAN.WPA2, 'Yutebtzuz6a2'))
 while not wlan.isconnected():
     machine.idle()
     pycom.rgbled(0xFF0000)
--- main.py
+++ main.py
@@ -6,30 +6,28 @@
  For measuring distance between object and itself.
 """""""""""""""""""""""""""""""""""""""""""""""""""
 
-
 #AUTHORINFO
 _author_ = "Joel Chapon"
 _email_  = "joel.chapon@student.kdg.be"
 _status_ = "Finished"
 _date_   = "2021-02-24"
 
-#LIBRARIES
+#IMPORTS
 from machine import UART
 
 #CONFIGURATIONS
 uart = UART(1)
-uart.init(bits=8, baudrate=9600, parity=None, stop=1, timeout_chars=100, pins('P3', 'P4'))
-DATA_HIGH = int(uart.read(1)[0])
-DATA_LOW = int(uart.read(1)[0])
-SUM = int(uart.read(1)[0])
+uart.init(9600, bits=8, parity=None, stop=1, timeout_chars=100, pins=('P3', 'P4'))
 
-#CODE
 while True:
     header_bytes = uart.read(1)
-    while header_bytes != b'\xff'):
-        header_bytes = uart.read(1)
-
-    global DATA_HIGH, DATA_LOW, SUM
-    if DATA_HIGH + DATA_LOW == SUM:
-        distance = (DATA_HIGH*256)+ DATA_LOW
-        print(distance)
+    while(header_bytes != b'\xff'):
+        header_bytes=uart.read(1)
+    data_high = int(uart.read(1)[0])
+    data_low = int(uart.read(1)[0])
+    data_sum = int(uart.read(1)[0])
+    sum = data_high + data_low
+    if (sum == data_sum + 1):
+        distance = (data_high*256 + data_low)
+        calc = "{:2.2f}".format(distance)
+        print(calc + " mm distance")
